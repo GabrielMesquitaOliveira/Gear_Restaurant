4ea3bb351722a409b43e9c93ecee6f6bd36e915d
gabriel <gabrielmesquita0503@gmail.com>
2024-11-27 13:17:55 -0300

    users profile

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->char('cpf', 11)->unique();
+            $table->char('phone',11)->unique();
+            $table->string('profile_picture')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
